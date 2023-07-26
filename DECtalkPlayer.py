f00b46b3cc73432d0f45a135279a96f0f3469fd2
TheColorOfFear <68704661+TheColorOfFear@users.noreply.github.com>
2023-07-26 11:20:45 -0500

    allow text to be hidden
    before every print statement, check if details is true. FOR DEBUG INFORMATION, DEBUG #AND# DETAILS MUST BE TRUE

--- DECtalkPlayer.py
+++ DECtalkPlayer.py
@@ -3,17 +3,19 @@ import subprocess
 import time
 import atexit
 
+details = True
+debug = False
+
 def exit_handler():
     try:
         for i in range(len(process_list)):
             process_list[i].kill()
     except:
-        print("error closing DECtalk windows")
+        if details:
+            print("error closing DECtalk windows")
 
-print("DECtalk Player v1.11 || 26 Jul 2023\n")
-
-details = True
-debug = False
+if details:
+    print("DECtalk Player v1.11 || 26 Jul 2023\n")
 
 #choose config file name
 conf_name = "index.cfg"
@@ -33,19 +35,21 @@ try:
             command_list.append([config[i+1][0:8].strip(), config[i+1][9:].strip()])
         if details:
             print("file names and dependencies:")
-            i = 0
-            for i_fake in range(len(command_list)):
-                if debug:
-                    print(i_fake, i, len(command_list))
-                if i < len(command_list):
+        i = 0
+        for i_fake in range(len(command_list)):
+            if debug and details:
+                print(i_fake, i, len(command_list))
+            if i < len(command_list):
+                if details:
                     print(command_list[i][1] + " - " + command_list[i][0])
-                    try:
-                        path_check = open(command_list[i][1], "r")
-                        path_check.close()
-                        i += 1
-                    except:
+                try:
+                    path_check = open(command_list[i][1], "r")
+                    path_check.close()
+                    i += 1
+                except:
+                    if details:
                         print("file \"" + command_list[i][1] + "\" not found")
-                        command_list.pop(i)
+                    command_list.pop(i)
         try:
             #run/stop speak_us.exe with all the files
             process_list = []
@@ -54,7 +58,8 @@ try:
                 try:
                     process_list.append(subprocess.Popen(command_list[i], start_new_session=True, shell=False))
                 except:
-                    print(command_list[i][1] + " - couldn't open \"" + command_list[i][0] + "\" window")
+                    if details:
+                        print(command_list[i][1] + " - couldn't open \"" + command_list[i][0] + "\" window")
             time.sleep(length)
         except:
             if debug:
@@ -65,6 +70,7 @@ try:
 except:
     if debug:
         raise
-    print("couldn't load \"" + conf_name + "\"")
-
-print("\nexiting program...")
+    if details:
+        print("couldn't load \"" + conf_name + "\"")
+if details:
+    print("\nexiting program...")
